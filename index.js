d1e555bb431167864bfeb1bdfcc0ca50717c6d1b
DMT <manyu.duttaluru@gmail.com>
2023-02-19 19:07:17 -0500

    master

--- index.js
+++ index.js
@@ -39,4 +39,30 @@ expressApp.get('/', async function (req, res) {
       
   })
   
+  expressApp.get('/shopee', async function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+      const time =moment().utcOffset("+05:30").format()
+      const message =  {
+          to: process.env.REC_MAIL,
+          from: process.env.SENDER_MAIL,
+          subject: "Shop-ee visited!", // Subject line
+          name : "Portfolio Monitor",
+          html: `<h1>Shop-ee Visited!</h1><p>Someone visited the Shop-ee site just now.</p><p>Time stamp : ${time}. </p><p>Raw time stamp : ${moment().format()}</p> <p>Visitor's IP address : ${req.ip}</p>`, // html body
+         
+        }
+      
+        sgMail.send(message).then((data )=>{
+          res.status(200).send("Done")
+        })
+        .catch((err)=>{
+            console.log(err);
+            
+            res.status(200).send("Some error.")
+        })
+        
+    })
+    
+
+
+
 expressApp.listen(process.env.PORT || 4999)
